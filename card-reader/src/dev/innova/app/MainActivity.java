0c036894828e9f71e2a219c978911f17368921b9
sajith4u <sajith.vijesekara@gmail.com>
2015-11-15 16:42:19 +0530

    Add new icon for app

--- card-reader/src/dev/innova/app/MainActivity.java
+++ card-reader/src/dev/innova/app/MainActivity.java
@@ -2,14 +2,23 @@ package dev.innova.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    ImageButton mobitelButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mobitelButton = (ImageButton) findViewById(R.id.mobitelButton);
+        mobitelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
